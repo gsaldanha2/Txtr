ccfb03773a3bdeeb0f8ec2fd8d2befa85621799e
Gregory Saldanha <gsaldanha2@gmail.com>
2015-11-23 12:12:46 -0800

    contact pictures now show

--- app/src/main/java/txtr/apps/armorg/com/txtr/MainActivity.java
+++ app/src/main/java/txtr/apps/armorg/com/txtr/MainActivity.java
@@ -1,6 +1,13 @@
 package txtr.apps.armorg.com.txtr;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,42 +17,129 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TabHost;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<Card> cards;
+    private List<Contact> contacts;
     private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar bar = (Toolbar) findViewById(R.id.toolbar);
+        bar.setTitle("Txtr");
+        setSupportActionBar(bar);
 
-        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
-        rv.setHasFixedSize(true);
+        RecyclerView cardsRv = (RecyclerView) findViewById(R.id.cards_rv);
+        RecyclerView contactsRv = (RecyclerView) findViewById(R.id.contacts_rv);
+
+        cardsRv.setHasFixedSize(true);
+        contactsRv.setHasFixedSize(true);
 
         layout = (RelativeLayout) findViewById(R.id.root);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
-        rv.setLayoutManager(llm);
+        cardsRv.setLayoutManager(llm);
+        LinearLayoutManager llm2 = new LinearLayoutManager(this);
+        contactsRv.setLayoutManager(llm2);
 
         initializeCards();
 
-        RVAdapter adapter = new RVAdapter(cards);
-        rv.setAdapter(adapter);;
+        RVCardsAdapter adapter = new RVCardsAdapter(cards);
+        cardsRv.setAdapter(adapter);
+
+        RVContactsAdapter contactsAdapter = new RVContactsAdapter(contacts);
+        contactsRv.setAdapter(contactsAdapter);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("cards");
+        tabSpec.setContent(R.id.cards_tab);
+        tabSpec.setIndicator("Cards");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("contacts");
+        tabSpec.setContent(R.id.contacts_tab);
+        tabSpec.setIndicator("Contacts");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("phrases");
+        tabSpec.setContent(R.id.phrases_tab);
+        tabSpec.setIndicator("Phrases");
+        tabHost.addTab(tabSpec);
     }
 
     public void initializeCards() {
         cards = new ArrayList<>();
         cards.add(new Card("When are you picking me up?", "510-367-2406"));
         cards.add(new Card("I'm done!", "602-563-9240"));
+
+        contacts = new ArrayList<>();
+
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (Integer.parseInt(cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Bitmap bit = openPhoto(pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
+                        contacts.add(new Contact(name, phoneNo, bit));
+                    }
+                    pCur.close();
+                }
+            }
+        }
+    }
+
+    public Bitmap openPhoto(String contactId) {
+        Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long.parseLong(contactId));
+        Uri photoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+        Cursor cursor = getContentResolver().query(photoUri,
+                new String[]{ContactsContract.Contacts.Photo.PHOTO}, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        try {
+            if (cursor.moveToFirst()) {
+                byte[] data = cursor.getBlob(0);
+                if (data != null) {
+                    return new BitmapFactory().decodeStream(new ByteArrayInputStream(data));
+                }
+            }
+        } finally {
+            cursor.close();
+        }
+        return null;
     }
 
     public void sendMessage(String msg, String contactNum) {
+
+        //send message here
+
         final Snackbar snackBar = Snackbar.make(layout, "Message sent", Snackbar.LENGTH_LONG);
         snackBar.setAction("DISMISS", new View.OnClickListener() {
             @Override
@@ -67,12 +161,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CardViewHolder> {
+    class Contact {
+        String contactName, contactNum;
+        Bitmap image;
+
+        Contact(String name, String num, Bitmap image) {
+            contactName = name;
+            contactNum = num;
+            this.image = image;
+        }
+    }
+
+    public class RVCardsAdapter extends RecyclerView.Adapter<RVCardsAdapter.CardViewHolder> {
         public class CardViewHolder extends RecyclerView.ViewHolder {
             CardView cv;
             TextView messageTv, contactTv;
 
-            CardViewHolder(View itemView) {
+            public CardViewHolder(View itemView) {
                 super(itemView);
                 cv = (CardView) itemView.findViewById(R.id.cv);
                 messageTv = (TextView) itemView.findViewById(R.id.message_tv);
@@ -87,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         List<Card> cards;
-        RVAdapter(List<Card> cards) {
+        public RVCardsAdapter(List<Card> cards) {
             this.cards = cards;
         }
 
@@ -97,13 +202,57 @@ public class MainActivity extends AppCompatActivity {
 
         public CardViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
             View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item, viewGroup, false);
-            CardViewHolder cvh = new CardViewHolder(v);
-            return cvh;
+            return new CardViewHolder(v);
         }
 
         public void onBindViewHolder(CardViewHolder cvh, int i) {
             cvh.messageTv.setText(cards.get(i).message);
             cvh.contactTv.setText(cards.get(i).contactNum);
+        }
+
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+    }
+
+    public class RVContactsAdapter extends RecyclerView.Adapter<RVContactsAdapter.CardViewHolder> {
+        public class CardViewHolder extends RecyclerView.ViewHolder {
+            CardView cv;
+            TextView contactNameTv, contactNumTv;
+            ImageView contactIv;
+
+            public CardViewHolder(View itemView) {
+                super(itemView);
+                cv = (CardView) itemView.findViewById(R.id.cv);
+                contactNameTv = (TextView) itemView.findViewById(R.id.contact_name_tv);
+                contactNumTv = (TextView) itemView.findViewById(R.id.contact_number_tv);
+                contactIv = (ImageView) itemView.findViewById(R.id.contact_iv);
+                cv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    }
+                });
+            }
+        }
+
+        List<Contact> contacts;
+        public RVContactsAdapter(List<Contact> contacts) {
+            this.contacts = contacts;
+        }
+
+        public int getItemCount() {
+            return contacts.size();
+        }
+
+        public CardViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact_item, viewGroup, false);
+            return new CardViewHolder(v);
+        }
+
+        public void onBindViewHolder(CardViewHolder cvh, int i) {
+            cvh.contactNameTv.setText(contacts.get(i).contactName);
+            cvh.contactNumTv.setText(contacts.get(i).contactNum);
+            cvh.contactIv.setImageBitmap(contacts.get(i).image);
         }
 
         public void onAttachedToRecyclerView(RecyclerView recyclerView) {
